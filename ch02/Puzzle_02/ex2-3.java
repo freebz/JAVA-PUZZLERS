ca24521759cc76b35cd3ba6b40cb751c20491d73
FreeBz <freebz@hananet.net>
2019-08-01 17:50:33 +0900

    2번째 퍼즐 - 변화를 위한 시간

---
+++ ch02/Puzzle_02/ex2-3.java
@@ -0,0 +1,7 @@
+import java.math.BigDecimal;
+public class Change {
+    public static void main(String args[]) {
+	System.out.println(new BigDecimal("2.00").
+			   subtract(new BigDecimal("1.10")));
+    }
+}
