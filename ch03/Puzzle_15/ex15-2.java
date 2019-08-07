1e913c99e709a9290ee2158d7e66cd387c05d963
FreeBz <freebz@hananet.net>
2019-08-07 15:08:22 +0900

    15번째 퍼즐 - 이상한 나라의 "Hello world"

---
+++ ch03/Puzzle_15/ex15-2.java
@@ -0,0 +1,7 @@
+// Questionable use of Unicode escape in Javadoc comment
+
+/**
+ * This method calls itself recursively. causing a
+ * <tt>StackOverflowError</tt> to be thrown.
+ * The algorithm is due to Peter von def Ah\u00E9.
+ */
---
+++ ch03/Puzzle_15/ex15-3.java
@@ -0,0 +1,5 @@
+/**
+ * This method calls itself recursively. causing a
+ * <tt>StackOverflowError</tt> to be thrown.
+ * The algorithm is due to Peter von def Ah&eacute;.
+ */
