24ef469292b7921d2dd9adecd35f2fea4c600974
FreeBz <freebz@hananet.net>
2019-08-14 11:31:23 +0900

    29번째 퍼즐 - 반복문의 신부

---
+++ ch04/Puzzle_29/ex29-2.java
@@ -0,0 +1,4 @@
+double i = 0.0 / 0.0;
+
+
+double i = Double.NaN;
---
+++ ch04/Puzzle_29/ex29-3.java
@@ -0,0 +1,6 @@
+class Test {
+    public static void main(String[] args) {
+	double i = 0.0 / 0.0;
+	System.out.println(i - i == 0);
+    }
+}
