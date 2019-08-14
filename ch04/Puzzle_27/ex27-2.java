54f871446de6bf1c061a5ed0b9a5fe5e6b2f7a10
FreeBz <freebz@hananet.net>
2019-08-14 11:08:56 +0900

    27번째 퍼즐 - 쉬프트 연산자와 변수 i

---
+++ ch04/Puzzle_27/ex27-2.java
@@ -0,0 +1,8 @@
+public class Shifty {
+    public static void main(String[] args) {
+	int distance = 0;
+	for (int val = -1; val != 0; val <<= 1)
+	    distance++;
+	System.out.println(distance);
+    }
+}
