73fd79450d6967215f375db740ae4c01e2ac5c4c
FreeBz <freebz@hananet.net>
2019-08-23 13:44:49 +0900

    71번째 퍼즐 - 임포트의 의무

---
+++ ch08/Puzzle_71/ex71-2.java
@@ -0,0 +1,6 @@
+import java.util.Arrays;
+class ImportDuty {
+    static void printArgs(Object... args) {
+	System.out.println(Arrays.toString(args));
+    }
+}
---
+++ ch08/Puzzle_71/ex71-3.java
@@ -0,0 +1,3 @@
+private static String toString(Object[] a) {
+    return Arrays.toString(a);
+}
